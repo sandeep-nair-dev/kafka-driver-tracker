1bc9b0954b11174b3ebe96b7f0705bb161279a56
Sandeep Nair <82336209+Hunterguns@users.noreply.github.com>
2025-07-03 07:24:25 +0530

    sn: producer config changes

--- driver-location-consumer/src/main/java/com/sandeep/driver_location_consumer/model/DriverLocation.java
+++ driver-location-consumer/src/main/java/com/sandeep/driver_location_consumer/model/DriverLocation.java
@@ -3,11 +3,49 @@ package com.sandeep.driver_location_consumer.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
 public class DriverLocation {
     private String driverId;
     private String longitude;
     private String latitude;
     private String timestamp;
+
+    public DriverLocation(String driverId, String longitude, String latitude, String timestamp) {
+        this.driverId = driverId;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.timestamp = timestamp;
+    }
+    public DriverLocation(){}
+
+    public String getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(String driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
 }
--- driver-location-producer/src/main/java/com/sandeep/driver_location_producer/config/KafkaTopicConfig.java
+++ driver-location-producer/src/main/java/com/sandeep/driver_location_producer/config/KafkaTopicConfig.java
@@ -2,13 +2,12 @@ package com.sandeep.driver_location_producer.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaTopicConfig {
-//    @Value("${spring.kafka.bootstrap-servers}")
-//    private String bootstrapServerAddress;
 
     @Value("${kafka.topic.driver-location}")
     private String topicName;
@@ -19,6 +18,7 @@ public class KafkaTopicConfig {
     @Value("${kafka.topic.replicas}")
     private int replicas;
 
+    @Bean
     public NewTopic driverLocationTopicConfig(){
         return TopicBuilder.name(topicName).partitions(partitions).replicas(replicas).build();
     }
